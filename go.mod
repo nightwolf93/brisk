5d145c07f042a035aad84af59dc4c94d14139dd0
Nightwolf <nico.style931@gmail.com>
2020-07-28 18:23:32 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nightwolf93/brisk
+
+go 1.13
+
+require github.com/gofiber/fiber v1.13.3 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "log"
+
+// version is the string ref of the version of brisk
+var version = "0.0.1"
+
+func main() {
+	log.Printf("starting brisk v%s ..", version)
+}
